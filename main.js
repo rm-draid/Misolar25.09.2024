ad9e641fee56f78bd19471b078fac63f3bda9bdf
rm-draid <rahimovmahmudjon89@gmail.com>
2024-09-25 02:46:34 +0300

    Misol

---
+++ main.js
@@ -0,0 +1,74 @@
+// begin 1.
+
+// let a = +prompt("A n kirit kutvoman:");
+// let b = +prompt("b n kirit kutvoman:");
+// let c = +prompt("c n  kirit kutvoman:");
+
+// console.log(typeof a);
+// let v = a * b * c;
+
+// alert(v)
+
+//  begin 9
+
+// let a = +prompt("A n kirit kutvoman:");
+// let b = +prompt("b n kirit kutvoman:");
+
+// let javob = a / b
+// alert (Natija)
+
+// begin 2
+
+// let r =  +prompt("r n kirit kutvoman:");
+
+// let l = 2 * Math.PI * r
+
+// alert (l)
+
+// begin 3
+
+// let a = +prompt("A n kirit kutvoman:");
+// let b = +prompt("b n kirit kutvoman:");
+
+// console.log(typeof a)
+// let javob = (a + b) / 2
+// alert(Natija)
+
+// begin 4
+
+// let a = +prompt("A  n kirit kutvoman:");
+// let b = +prompt("b  n kirit kutvoman:");
+
+// let javob = Math.sqrt(a * b)
+
+// alert(Natija)
+
+//  begin 6
+
+// let cm = +prompt("cm n kirit kutvoman:");
+
+// let m = cm / 100
+
+// alert(m)
+
+// begin 7
+
+// let kg = +prompt("kg n kirit kutvoman:");
+
+// let t = kg / 1000
+// alert(t)
+
+// begin 8
+
+// let bayt = +prompt("bayt n kirit kutvoman:");
+// let kb = bayt / 1024
+
+// alert (kb)
+
+// begin 10
+
+let a = +prompt("A n kirit kutvoman:");
+let b = +prompt("b n kirit kutvoman:");
+
+let javob = a % b;
+alert(Natija);
